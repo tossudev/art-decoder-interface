97778a45e64279fd66909527622a541daa94c043
Joonatan :) <tossudev@gmail.com>
2026-04-23 12:32:44 +0300

    add: form handler and input area html

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 	//"io"
 )
 
@@ -13,7 +14,20 @@ func main() {
 	}
 	*/
 
+	http.HandleFunc("/input", FormHandler)
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.ListenAndServe(":8080", nil)
 
+}
+
+
+func FormHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	input := r.FormValue("input")
+
+	fmt.Println(input)
 }
